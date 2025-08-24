f23fb7be2c9e690e4bd6fbc0265ecfb259a14801
Florian Gamböck <mail@floga.de>
2025-08-24 17:02:18 +0200

    Handle cache persisting in module

--- src/cache/mod.rs
+++ src/cache/mod.rs
@@ -1,12 +1,78 @@
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Read, Write};
+use std::path::Path;
+
 use crate::cache::v0::{CacheOnDisk, CacheOnDiskBorrowed};
 use crate::{DedupCache, FileWithChunks};
 
 mod v0;
 
-pub(crate) fn from_str(s: &str) -> serde_json::Result<Vec<FileWithChunks>> {
-    serde_json::from_str::<CacheOnDisk>(s).map(CacheOnDisk::into_inner)
+/// Reads a cache file from the specified path and returns its content as a `String`.
+///
+/// This function can handle regular text files as well as compressed files with
+/// a `.zst` extension (Zstandard-compressed files). If the file is compressed,
+/// it will automatically decompress it before returning the content.
+fn read_cache_file(path: &Path) -> std::io::Result<String> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let mut reader: Box<dyn Read> = if path.extension() == Some("zst".as_ref()) {
+        let decoder = zstd::Decoder::with_buffer(reader)?;
+        Box::new(decoder)
+    } else {
+        Box::new(reader)
+    };
+
+    let mut buffer = String::new();
+    reader.read_to_string(&mut buffer)?;
+
+    Ok(buffer)
 }
 
-pub(crate) fn to_writer(writer: impl std::io::Write, cache: &DedupCache) -> serde_json::Result<()> {
-    serde_json::to_writer(writer, &CacheOnDiskBorrowed::from(cache))
+/// Creates a cache writer for the specified path.
+///
+/// This function creates a writer that writes to the given path. If the file extension of the path
+/// is `.zst`, the writer will compress the data using Zstandard compression. Otherwise, it will
+/// use a buffered writer without compression.
+fn get_cache_writer(path: &Path) -> std::io::Result<Box<dyn Write>> {
+    let file = File::create(&path)?;
+    let writer = BufWriter::new(file);
+
+    Ok(if path.extension() == Some("zst".as_ref()) {
+        let encoder = zstd::Encoder::new(writer, 0)?.auto_finish();
+        Box::new(encoder)
+    } else {
+        Box::new(writer)
+    })
+}
+
+pub(crate) fn read_from_file(path: impl AsRef<Path>) -> Vec<FileWithChunks> {
+    let path = path.as_ref();
+
+    let cache_from_file = read_cache_file(path);
+    cache_from_file
+        .ok()
+        .and_then(|s| {
+            serde_json::from_str::<CacheOnDisk>(&s)
+                .map(CacheOnDisk::into_inner)
+                .ok()
+        })
+        .unwrap_or_default()
+}
+
+pub(crate) fn write_to_file(path: impl AsRef<Path>, cache: &DedupCache) {
+    let path = path.as_ref();
+
+    if path.file_name().is_none() {
+        return;
+    }
+
+    std::fs::create_dir_all(path.parent().unwrap()).unwrap();
+
+    let writer = get_cache_writer(&path);
+
+    writer
+        .map(|writer| serde_json::to_writer(writer, &CacheOnDiskBorrowed::from(cache)))
+        .unwrap()
+        .unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -256,45 +256,6 @@ fn read_at_chunk(file: &File, offset: u64, len: usize) -> std::io::Result<Vec<u8
     Ok(buf)
 }
 
-/// Reads a cache file from the specified path and returns its content as a `String`.
-///
-/// This function can handle regular text files as well as compressed files with
-/// a `.zst` extension (Zstandard-compressed files). If the file is compressed,
-/// it will automatically decompress it before returning the content.
-fn read_cache_file(path: &Path) -> std::io::Result<String> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-
-    let mut reader: Box<dyn Read> = if path.extension() == Some("zst".as_ref()) {
-        let decoder = zstd::Decoder::with_buffer(reader)?;
-        Box::new(decoder)
-    } else {
-        Box::new(reader)
-    };
-
-    let mut buffer = String::new();
-    reader.read_to_string(&mut buffer)?;
-
-    Ok(buffer)
-}
-
-/// Creates a cache writer for the specified path.
-///
-/// This function creates a writer that writes to the given path. If the file extension of the path
-/// is `.zst`, the writer will compress the data using Zstandard compression. Otherwise, it will
-/// use a buffered writer without compression.
-fn get_cache_writer(path: &Path) -> std::io::Result<Box<dyn Write>> {
-    let file = File::create(&path)?;
-    let writer = BufWriter::new(file);
-
-    Ok(if path.extension() == Some("zst".as_ref()) {
-        let encoder = zstd::Encoder::new(writer, 0)?.auto_finish();
-        Box::new(encoder)
-    } else {
-        Box::new(writer)
-    })
-}
-
 /// A lazily initialized optional value that can be serialized/deserialized via `Option<T>`.
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 #[serde(from = "Option<T>")]
@@ -505,15 +466,7 @@ impl DedupCache {
 
     /// Reads cache entries from a file. Supports optional zstd compression based on extension.
     fn read_from_file(&mut self, path: impl AsRef<Path>) {
-        let path = path.as_ref();
-
-        let cache_from_file = {
-            let cache_from_file = read_cache_file(path);
-            cache_from_file
-                .ok()
-                .and_then(|s| cache::from_str(s.as_str()).ok())
-                .unwrap_or_default()
-        };
+        let cache_from_file = cache::read_from_file(path);
 
         for x in cache_from_file {
             self.insert(x.path.clone(), x);
@@ -522,20 +475,7 @@ impl DedupCache {
 
     /// Writes the cache to a file, optionally compressing with zstd if extension suggests.
     fn write_to_file(&self, path: impl AsRef<Path>) {
-        let path = path.as_ref();
-
-        if path.file_name().is_none() {
-            return;
-        }
-
-        std::fs::create_dir_all(path.parent().unwrap()).unwrap();
-
-        let writer = get_cache_writer(&path);
-
-        writer
-            .map(|writer| cache::to_writer(writer, self))
-            .unwrap()
-            .unwrap();
+        cache::write_to_file(path, self);
     }
 
     /// Iterates over all chunks, yielding the chunk hash, enriched `FileChunk` with path, and a
